cd85853496851717e373ef53b180c488582ea379
bjmhe <hi@bjmhe.me>
2026-05-29 01:44:55 +0800

    chore: enhance Vite configuration with detailed settings for linting, formatting, and packaging; update package.json to maintain module and types fields

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,44 @@
-import { nodeLib } from "viteplus-preset-bjmhe";
+import { defineConfig } from "vite-plus";
+import * as attw from "@arethetypeswrong/core";
+import * as publint from "publint";
+import * as publintUtils from "publint/utils";
+import ApiSnapshot from "tsnapi/rolldown";
 
-export default nodeLib();
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  pack: {
+    attw: {
+      enabled: false,
+      profile: "esm-only",
+      module: attw,
+      level: "warn",
+    },
+    deps: { onlyBundle: [] },
+    dts: {
+      tsgo: true,
+    },
+    entry: "src/index.ts",
+    exports: {
+      packageJson: true,
+      legacy: true,
+    },
+    platform: "node",
+    plugins: [ApiSnapshot()],
+    publint: {
+      enabled: "ci-only",
+      module: [publint, publintUtils],
+    },
+  },
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+    ignorePatterns: ["__snapshots__/**/*", "dist/**/*"],
+  },
+  fmt: {
+    ignorePatterns: ["__snapshots__/**/*", "dist/**/*"],
+  },
+});
